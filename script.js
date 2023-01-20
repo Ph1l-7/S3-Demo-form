e68eb336c27a8392617f33afcbfd36cb060e590c
PH1L-7 <lussier.philippe@gmail.com>
2023-01-20 14:56:15 -0500

    element formulaire added, JS still basic

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function valider(){
-    //let nom = document.forms[0].
-    alert("Uh Oh...")
-    return false; //Empeche envoie du form
+    let mdp1 = document.forms[0]["mdp1"].value;
+    let mdp2 = document.forms[0]["mdp2"].value;
+    
+    if(mdp1 === mdp2){ //proceed
+        //minimum 8 char
+        if (mdp1.length < 8){
+            alert("8 chars minimum!")
+            return false;
+        }
+        //special char = WRONG!
+        if (mdp1.includes("%",0)){
+            alert("Wrong!")
+            return false;
+        }
+        return true;
+    }else{ //stops
+        alert("Les MDP ne sont pas identiques")
+        return false; //Empeche envoie du form
+    }
+    
+    
 }
